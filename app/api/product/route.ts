135d06d33a00cd17e1104edd857c0a710632ce09
.kirill <143953592+godmozzarella@users.noreply.github.com>
2026-02-22 21:33:23 +0300

    Update route.ts

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const response = await fetch(
-    `https://app.tablecrm.com/api/v1/nomenclature/?token=${process.env.TABLECRM_TOKEN}`
+    `https://app.tablecrm.com/api/v1/nomenclature/?token=af1874616430e04cfd4bce30035789907e899fc7c3a1a4bb27254828ff304a77`
   );
 
   const data = response.headers.get("content-type")?.includes("application/json")
