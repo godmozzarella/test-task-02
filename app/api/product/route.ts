649aa1b001b94ccd8765c31493ae56143b1636f8
.kirill <143953592+godmozzarella@users.noreply.github.com>
2026-02-22 21:40:32 +0300

    Update route.ts

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
   const body = await req.json();
 
   const response = await fetch(
-    `https://app.tablecrm.com/api/v1/nomenclature/?token=${process.env.TABLECRM_TOKEN}`,
+    `https://app.tablecrm.com/api/v1/nomenclature/?token=af1874616430e04cfd4bce30035789907e899fc7c3a1a4bb27254828ff304a77`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
